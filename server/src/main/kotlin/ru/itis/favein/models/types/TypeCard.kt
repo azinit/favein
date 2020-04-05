6dd39e43d104fa3a832c040a2841f4f8e6c2fb5c
Ilya Azin <martis.azin@gmail.com>
2020-04-05 16:18:29 +0300

    simplify data classes
    #bl

--- server/src/main/kotlin/ru/itis/favein/models/types/TypeCard.kt
+++ server/src/main/kotlin/ru/itis/favein/models/types/TypeCard.kt
@@ -1,15 +1,13 @@
 package ru.itis.favein.models.types
 
-import java.io.Serializable
-
 data class TypeCardCreate(
         val name: String,
         val listId: Long
-) : Serializable
+)
 
 data class TypeCardUpdate(
         val id: Long,
         val name: String? = null,
         val description: String? = null,
         val content: String? = null
-) : Serializable
+)
