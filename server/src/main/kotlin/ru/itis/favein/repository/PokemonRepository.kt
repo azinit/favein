2288224fb5ad2a8517835a82f5e8c628722c1f6e
Ilya Azin <martis.azin@gmail.com>
2020-04-13 00:41:24 +0300

    remove graphql
    #bl #remove-graphql

--- server/src/main/kotlin/ru/itis/favein/repository/PokemonRepository.kt
+++
@@ -1,7 +0,0 @@
-package ru.itis.favein.repository
-
-import org.springframework.data.repository.CrudRepository
-import ru.itis.favein.models.Pokemon
-
-interface PokemonRepository: CrudRepository<Pokemon, Long> {
-}
