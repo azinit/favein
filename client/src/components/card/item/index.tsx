d9f18a0e53f038fbd4e99ca41145de957351c66b
Ilya Azin <martis.azin@gmail.com>
2020-04-15 07:22:40 +0300

    extend card-component
    #mvp

--- client/src/components/card/item/index.tsx
+++ client/src/components/card/item/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Card as BCard, Breadcrumb } from 'react-bootstrap'
+import Label from '../../label'
+import Comment from '../../comment'
+import Rate from '../../rate'
+
 type Props = {
     card: ICard;
 }
@@ -17,14 +21,29 @@ const Card = (props: Props) => {
                 <BCard.Title className="text-center">{name}</BCard.Title>
                 <BCard.Subtitle className="text-secondary text-center mb-4">{author.username} ({author.email})</BCard.Subtitle>
                 <BCard.Text>
+                    {labels &&
+                        <div className="labels d-flex justify-content-center mb-4">
+                            {labels.map(l => <Label key={l.id} label={l} />)}
+                        </div>
+                    }
                     {description &&
-                        <div className="summary mb-4 border border-secondary p-4">
+                        <div className="summary mb-4 border border-secondary rounded p-4">
                             {description}
                         </div>
                     }
-                    <div className="content">
+                    <div className="content mb-4">
                         {content}
                     </div>
+                    {rates &&
+                        <div className="rates d-flex mb-4">
+                            {rates.map(r => <Rate key={r.id} rate={r} />)}
+                        </div>
+                    }
+                    {comments &&
+                        <div className="comments d-flex">
+                            {comments.map(c => <Comment key={c.id} comment={c} />)}
+                        </div>
+                    }
                 </BCard.Text>
             </BCard.Body>
         </BCard>
