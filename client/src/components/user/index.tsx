5dc8aca0d771d93c15203c9ef434e9c984f37e7f
Ilya Azin <martis.azin@gmail.com>
2020-04-15 06:53:24 +0300

    add user component and fix demo styles
    #mvp

--- client/src/components/user/index.tsx
+++ client/src/components/user/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { PersonFill } from 'react-bootstrap-icons'
+import Card from 'react-bootstrap/Card'
 type Props = {
     user: IUser;
 }
@@ -8,7 +9,16 @@ const User = (props: Props) => {
     const { email, admin, id, username, roles } = props.user
     return (
         <div>
-            { username }
+            <Card className="user">
+                <Card.Header>
+                    <PersonFill className="mr-2" />
+                    {username}
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>{email} </Card.Title>
+                    <Card.Subtitle className="text-muted text-light">{roles.join(', ')} </Card.Subtitle>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
