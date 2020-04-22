14f79cf6182e24d05ae1ca5b2f371e740c9a48f2
Ilya Azin <martis.azin@gmail.com>
2020-04-23 01:32:33 +0300

    modify header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -4,7 +4,7 @@ import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from 'react-boots
 const Header = () => {
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="/home">Favein</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
