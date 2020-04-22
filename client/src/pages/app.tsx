e1399ce2473ed2640562897f35fb50c4f73845bd
Ilya Azin <martis.azin@gmail.com>
2020-04-23 01:29:40 +0300

    add users page and header

--- client/src/pages/app.tsx
+++ client/src/pages/app.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import Loader from 'components/loader'
-import './app.scss'
 import { store } from 'store';
+import Loader from 'components/loader'
+import Header from 'components/header'
+import './app.scss'
 
 const HomePage = React.lazy(() => import('./home'))
 const ProfilePage = React.lazy(() => import('./profile'))
 const AdminPage = React.lazy(() => import('./admin'))
+const UsersPage = React.lazy(() => import('./users'))
 
 // TODO: withAuth
 // TODO: withStore
@@ -17,12 +19,14 @@ function App() {
   return (
     <Provider store={store}>
       <div className="favein-app">
+        <Header/>
         <BrowserRouter>
           <React.Suspense fallback={<Loader className="overlay" />}>
             <Switch>
               <Route path="/home" component={HomePage} />
               <Route path="/profile/:id" component={ProfilePage} />
               <Route path="/admin" component={AdminPage} />
+              <Route path="/users" component={UsersPage} />
               <Redirect to="admin" />
             </Switch>
           </React.Suspense>
---
+++ client/src/pages/users/index.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import User from 'components/user'
+import API from 'api'
+
+const UsersPage = () => {
+    const [users, setUsers] = useState<IUser[]>([])
+
+    useEffect(() => {
+        API.users.readList().then(response => setUsers(response.data))
+    }, [])
+
+    return (
+        <div>
+            <section className="jumbotron text-center">
+                <h2>Пользователи</h2>
+            </section>
+            <section className="jumbotron bg-white">
+                <div className="container">
+                    {users.map(u => (
+                        <User user={u} />
+                    ))}
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default UsersPage
