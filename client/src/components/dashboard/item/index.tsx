62c2b4c37a22c3a107781880c92dd91c938cb914
Ilya Azin <martis.azin@gmail.com>
2020-04-25 16:17:08 +0300

    add inputs to dashboard-item

--- client/src/components/dashboard/item/index.tsx
+++ client/src/components/dashboard/item/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import { dashboardsSlice } from 'store/entities'
 import { deleteEntity } from 'store/entities/service'
-import './index.scss'
+import Input from 'components/input'
 import DashboardActions from './dashboard-actions'
+import './index.scss'
 
+const { } = dashboardsSlice.actions
 type Props = {
     dashboard: IDashboard;
     showAuthor?: boolean;
@@ -15,10 +18,18 @@ type Props = {
 const DashboardItem = (props: Props) => {
     const { dashboard, showAuthor = true, showActions = false } = props
     const { author, background, description, id, name } = dashboard
+    const [state, setState] = useState<MutationState>("preview")
     const dispatch = useDispatch()
 
+    const onChange: OnChange = (e) => {
+
+    }
     const onEdit = () => {
+        setState('edit')
         console.log('EDIT: impl')
+    }
+    const onCancel = () => {
+        setState('edit')
     }
 
     const onDelete = () => {
@@ -30,14 +41,33 @@ const DashboardItem = (props: Props) => {
             <Card.Img src={background} alt={`${author.username}/${name}`} />
             <Link to={`/dashboards/${id}`} className="dashboard-link">
                 <Card.ImgOverlay>
-                    <Card.Title>{name}</Card.Title>
-                    <Card.Text>{description}</Card.Text>
+                    <Card.Title>
+                        <Input
+                            name="name"
+                            value={name}
+                            onChange={onChange}
+                            mutationState={state}
+                        />
+                    </Card.Title>
+                    <Card.Text>
+                        <Input
+                            name="description"
+                            value={description}
+                            onChange={onChange}
+                            mutationState={state}
+                        />
+                    </Card.Text>
                     {showAuthor && (
                         <Card.Text>{author.username}&nbsp;&lt;{author.email}&gt;</Card.Text>
                     )}
                 </Card.ImgOverlay>
             </Link>
-            {showActions && <DashboardActions onDelete={onDelete} onEdit={onEdit} />}
+            {showActions && (
+                <DashboardActions
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                />
+            )}
         </Card>
     )
 }
