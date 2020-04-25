3af1afc7d1fbb7d9d37e1763b6bd4b9c1ba6c188
Ilya Azin <martis.azin@gmail.com>
2020-04-25 16:16:59 +0300

    modify and extend dashboard actions component

--- client/src/components/dashboard/item/dashboard-actions/index.tsx
+++ client/src/components/dashboard/item/dashboard-actions/index.tsx
@@ -1,29 +1,34 @@
 import React from 'react'
 import { Button, ButtonGroup } from 'react-bootstrap'
-import { PencilSquare, Trash } from 'react-bootstrap-icons'
+import { PencilSquare, Trash, X, FileEarmarkCheck } from 'react-bootstrap-icons'
 
 type Props = {
-    onDelete: Function;
-    onEdit: Function;
+    onDelete?: Function;
+    onEdit?: Function;
+    onCancel?: Function;
+    onSave?: Function;
 }
 
 const DashboardActions = (props: Props) => {
-    const { onDelete, onEdit } = props
+    const { onDelete, onEdit, onCancel, onSave } = props
+    const buttons = [
+        { id: 1, onClick: onEdit, className: 'edit', component: <PencilSquare /> },
+        { id: 2, onClick: onDelete, className: 'delete', component: <Trash /> },
+        { id: 3, onClick: onCancel, className: 'cancel', component: <X /> },
+        { id: 4, onClick: onSave, className: 'save', component: <FileEarmarkCheck /> },
+    ]
     return (
         <div className="dashboard-actions" style={{ width: '100%' }}>
             <ButtonGroup style={{ width: '100%' }}>
-                <Button
-                    className="rounded-0 action edit"
-                    variant="dark"
-                    onClick={() => onEdit()}
-                    children={<PencilSquare />}
-                />
-                <Button
-                    className="rounded-0 action delete"
-                    variant="dark"
-                    onClick={() => onDelete()}
-                    children={<Trash />}
-                />
+                {buttons.filter(btn => !!btn.onClick).map(({ onClick, className, component, id }) => (
+                    <Button
+                        key={id}
+                        className={`rounded-0 action ${className}`}
+                        variant="dark"
+                        onClick={() => onClick!()}
+                        children={component}
+                    />
+                ))}
             </ButtonGroup>
         </div>
     )
