cb0def8d709c25d5f457b7b4a96652373b11c19a
Ilya Azin <martis.azin@gmail.com>
2020-05-02 15:13:56 +0300

    modify card-sheet display

--- client/src/components/card/sheet/index.tsx
+++ client/src/components/card/sheet/index.tsx
@@ -4,6 +4,7 @@ import Label from 'components/label'
 import Comment from 'components/comment'
 import Rate from 'components/rate'
 import Markdown from 'components/markdown'
+import { ChatSquare, PersonFill } from 'react-bootstrap-icons'
 import './index.scss'
 
 type Props = {
@@ -14,6 +15,12 @@ const CardSheet = (props: Props) => {
     const { author, content, comments, dashboard, description, id, labels, list, name, rates } = props.card
     const dashboardLink = `/dashboards/${dashboard.id}`
     const listLink = `${dashboardLink}#list-${list.id}`
+    const rate = {
+        author: {},
+        id: -1,
+        value: (!rates.length) ? 0 : Math.ceil(rates.map(r => r.value).reduce((a, b) => a + b) / rates.length)
+    } as IRate
+
     return (
         <Card className="card-sheet">
             <Breadcrumb>
@@ -23,34 +30,55 @@ const CardSheet = (props: Props) => {
             </Breadcrumb>
             <Card.Body>
                 <Card.Title className="text-center">{name}</Card.Title>
-                <Card.Subtitle className="text-secondary text-center mb-4">{author.username} ({author.email})</Card.Subtitle>
-                <Card.Text>
-                    <div>
-                        {labels && (
-                            <div className="labels d-flex justify-content-center mb-4">
-                                {labels.map(l => <Label key={l.id} label={l} />)}
-                            </div>
-                        )}
-                        {description && (
-                            <Alert variant="info">
-                                {description}
-                            </Alert>
-                        )}
-                        <div className="content mb-4">
-                            <Markdown source={content} />
+                <Card.Subtitle className="text-secondary text-center mb-2">{author.username} ({author.email})</Card.Subtitle>
+                <section className="text-center">
+                    <a className='link-reset' href="#comments">{comments.length}&nbsp;<ChatSquare /></a>
+                    &nbsp;
+                    &nbsp;
+                    <a className='link-reset' href="#rates">{rates.length}<PersonFill /></a>
+                </section>
+                <section>
+                    {labels && (
+                        <div className="labels d-flex justify-content-center">
+                            {labels.map(l => <Label key={l.id} label={l} />)}
                         </div>
-                        {rates && (
-                            <div className="rates d-flex mb-4">
-                                {rates.map(r => <Rate key={r.id} rate={r} />)}
-                            </div>
-                        )}
-                        {comments && (
-                            <div className="comments d-flex">
-                                {comments.map(c => <Comment key={c.id} comment={c} />)}
-                            </div>
-                        )}
+                    )}
+                    {description && (
+                        <Alert variant="info">
+                            {description}
+                        </Alert>
+                    )}
+                </section>
+                <hr />
+                <section className="card-content">
+                    <div className="content mb-4">
+                        <Markdown source={content} />
                     </div>
-                </Card.Text>
+                </section>
+                <hr />
+                <section className="social-block">
+                    <div className="rates">
+                        <h4 id="rates">Social rate</h4>
+                        <div className='d-flex align-items-center'>
+                            <Rate rate={rate} showAuthor={false} />
+                            {rates.length ? (
+                                <div className='text-muted'>({rates.length}<PersonFill />)</div>
+                            ) : (
+                                    <span className='text-muted'>Be first!</span>
+                                )}
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="comments">
+                        <h4 id="comments">
+                            Comments&nbsp;
+                                <span className="text-muted">{comments.length}</span>
+                        </h4>
+                        <div className='comments-list d-flex'>
+                            {comments.map(c => <Comment key={c.id} comment={c} />)}
+                        </div>
+                    </div>
+                </section>
             </Card.Body>
         </Card>
     )
