d4759687d3ab8b48ee381e5b6cb285f20638ae77
Ilya Azin <martis.azin@gmail.com>
2020-05-10 13:56:08 +0300

    add fave endpoints

--- server/src/main/kotlin/ru/itis/favein/api/UserController.kt
+++ server/src/main/kotlin/ru/itis/favein/api/UserController.kt
@@ -10,6 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import ru.itis.favein.models.User
 import ru.itis.favein.models.UserDTO
+import ru.itis.favein.repository.CardRepository
 import ru.itis.favein.repository.UserRepository
 import javax.validation.Valid
 
@@ -21,7 +22,9 @@ import javax.validation.Valid
 @PreAuthorize("isAuthenticated()")
 class UserController(
         @Autowired
-        private val userRepository: UserRepository
+        private val userRepository: UserRepository,
+        @Autowired
+        private val cardRepository: CardRepository
 ) {
     @ApiOperation("Получить список пользователей")
     @GetMapping
@@ -88,6 +91,56 @@ class UserController(
         val entity = userRepository.findById(id)
         if (entity.isPresent) {
             userRepository.deleteById(id)
+            return ResponseEntity(HttpStatus.OK)
+        }
+        return ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    fun processFaves(faves: String): List<Long> {
+        if (faves == "") {
+            return listOf()
+        }
+        return faves.split(" ").map { it.toLong() }
+    }
+
+    @ApiOperation("Добавить карточку в избранное")
+    @PutMapping("{user-id}/faves/add/{card-id}")
+    fun addFave(
+            @ApiParam("Уникальный идентификатор пользователя", required = true)
+            @PathVariable("user-id") userId: Long,
+            @ApiParam("Уникальный идентификатор карточки", required = true)
+            @PathVariable("card-id") cardId: Long
+    ): ResponseEntity<HttpStatus> {
+        val userEntity = userRepository.findById(userId)
+        val cardEntity = cardRepository.findById(cardId)
+        if (userEntity.isPresent && cardEntity.isPresent) {
+            val user = userEntity.get()
+            val faveIds = processFaves(user.faves).toMutableSet()
+            faveIds += cardId
+            user.faves = faveIds.toSortedSet().joinToString(" ")
+            userRepository.save(user)
+            return ResponseEntity(HttpStatus.OK)
+        }
+        return ResponseEntity(HttpStatus.NOT_FOUND)
+    }
+
+    @ApiOperation("Удалить карточку из избранного")
+    @PutMapping("{user-id}/faves/delete/{card-id}")
+    fun deleteFave(
+            @ApiParam("Уникальный идентификатор пользователя", required = true)
+            @PathVariable("user-id") userId: Long,
+            @ApiParam("Уникальный идентификатор карточки", required = true)
+            @PathVariable("card-id") cardId: Long
+    ): ResponseEntity<HttpStatus> {
+        val userEntity = userRepository.findById(userId)
+        val cardEntity = cardRepository.findById(cardId)
+        if (userEntity.isPresent && cardEntity.isPresent) {
+            val user = userEntity.get()
+            val faveIds = processFaves(user.faves)
+                    .filter { it != cardId }
+                    .toMutableSet()
+            user.faves = faveIds.toSortedSet().joinToString(" ")
+            userRepository.save(user)
             return ResponseEntity(HttpStatus.OK)
         }
         return ResponseEntity(HttpStatus.NOT_FOUND)
